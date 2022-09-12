77c766d55e79bc0255ae90ca51015b738502fe13
Shahid Ameen <iamshahidameen@gmail.com>
2022-09-12 21:04:24 +0500

    react router setup for the app

--- src/App.js
+++ src/App.js
@@ -2,22 +2,46 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Navbar, Sidebar, Footer } from './components';
 
-import styled from 'styled-components';
-
-const Button = styled.button`
-  color: red;
-  a {
-    color: green;
-  }
-`;
+import {
+  Home,
+  About,
+  Cart,
+  Products,
+  SingleProduct,
+  Checkout,
+  Error,
+} from './pages';
 
 function App() {
   return (
     <>
       <h4>comfy sloth starter</h4>
-      <Button>
-        <a href="https://www.google.com">Google</a>
-      </Button>
+      <Router>
+        <Navbar />
+        <Sidebar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/cart">
+            <Cart />
+          </Route>
+          <Route exact path="/products">
+            <Products />
+          </Route>
+          <Route exact path="/products/:id" children={SingleProduct} />
+          <Route exact path="/checkout">
+            <Checkout />
+          </Route>
+          <Route exact path="*">
+            <Error />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </>
   );
 }

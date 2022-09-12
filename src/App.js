a17235275454cc0017b110f6e2ee2b2f29b5107d
Shahid Ameen <iamshahidameen@gmail.com>
2022-09-12 20:40:45 +0500

    tested styled components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Navbar, Sidebar, Footer } from './components';
+
+import styled from 'styled-components';
+
+const Button = styled.button`
+  color: red;
+  a {
+    color: green;
+  }
+`;
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <>
+      <h4>comfy sloth starter</h4>
+      <Button>
+        <a href="https://www.google.com">Google</a>
+      </Button>
+    </>
+  );
 }
 
-export default App
+export default App;

242548a374a9365a26896c8a7d624e1f9639102f
Shahid Ameen <iamshahidameen@gmail.com>
2022-09-29 20:54:29 +0500

    stars functionality completed manually

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,9 +1,62 @@
-import React from 'react'
-import styled from 'styled-components'
-import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs'
-const Stars = () => {
-  return <h4>stars</h4>
-}
+import React from 'react';
+import styled from 'styled-components';
+import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs';
+const Stars = ({ stars, reviews }) => {
+  console.log(stars, 'stars', reviews, 'reviews');
+  return (
+    <Wrapper>
+      <div className="stars">
+        {/* Stars Start */}
+        <span>
+          {stars >= 1 ? (
+            <BsStarFill />
+          ) : stars <= 0.5 ? (
+            <BsStarHalf />
+          ) : (
+            <BsStar />
+          )}
+          {stars >= 1 ? (
+            <BsStarFill />
+          ) : stars <= 0.5 ? (
+            <BsStarHalf />
+          ) : (
+            <BsStar />
+          )}
+          {stars >= 2 ? (
+            <BsStarFill />
+          ) : stars <= 0.5 ? (
+            <BsStarHalf />
+          ) : (
+            <BsStar />
+          )}
+          {stars >= 3 ? (
+            <BsStarFill />
+          ) : stars <= 0.5 ? (
+            <BsStarHalf />
+          ) : (
+            <BsStar />
+          )}
+          {stars >= 4 ? (
+            <BsStarFill />
+          ) : stars <= 0.5 ? (
+            <BsStarHalf />
+          ) : (
+            <BsStar />
+          )}
+          {stars >= 5 ? (
+            <BsStarFill />
+          ) : stars <= 0.5 ? (
+            <BsStarHalf />
+          ) : (
+            <BsStar />
+          )}
+        </span>
+        {/* End of Stars */}
+      </div>
+      <p className="reviews">({reviews} Customer Reviews)</p>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.div`
   display: flex;
@@ -18,5 +71,5 @@ const Wrapper = styled.div`
     margin-bottom: 0;
   }
   margin-bottom: 0.5rem;
-`
-export default Stars
+`;
+export default Stars;
